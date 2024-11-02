437c7020d33a073251c51b115fc9c01002d3f512
Redhun <redhuns123@gmail.com>
2024-11-02 23:22:25 +0530

    update,delete,search fuctions are added

--- src/main/java/com/redhun/SpringEcom/service/ProductsService.java
+++ src/main/java/com/redhun/SpringEcom/service/ProductsService.java
@@ -4,6 +4,7 @@ import com.redhun.SpringEcom.model.Products;
 import com.redhun.SpringEcom.repo.ProductsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -18,7 +19,7 @@ public class ProductsService {
 
     public Products getProduct(int id) {
 
-        return  repo.findById(id).orElse(null);
+        return  repo.findById(id).orElse( new Products(-1));
     }
 
     public Products addProduct(Products product, MultipartFile image) throws IOException {
@@ -27,5 +28,21 @@ public class ProductsService {
         product.setImageType(image.getContentType());
         product.setImageData(image.getBytes());
         return  repo.save(product);
+    }
+
+    public Products updateProduct(Products product, MultipartFile imageFile) throws IOException {
+
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return  repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
+    public List<Products> searchProducts(String keyword) {
+        return  repo.searchProducts(keyword);
     }
 }
